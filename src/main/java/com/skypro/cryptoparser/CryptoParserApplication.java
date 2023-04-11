6e94380a05b43c45b6db2cce24f6b7ea2146e2bf
Alexey Volkov <alexey.volkov@maxoptra.com>
2023-04-11 09:54:38 +0200

    Initial commit

---
+++ src/main/java/com/skypro/cryptoparser/CryptoParserApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.cryptoparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoParserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CryptoParserApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/skypro/cryptoparser/CryptoParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.cryptoparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoParserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
